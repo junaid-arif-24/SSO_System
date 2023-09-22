4597576633d46836ce3fa87261e500385f9f180b
junaid-arif-24 <mohdjunaid7078@gmail.com>
2023-09-22 23:23:23 +0530

    fixed CORS ERROR

--- Backend/index.js
+++ Backend/index.js
@@ -20,11 +20,7 @@ app.use(
 app.use(express.json());
 app.use(passport.initialize());
 app.use(passport.session());
-app.use(cors({
-    origin: `${process.env.CLIENT_URL}`,
-    methods: "GET,POST,PUT,DELETE",
-    credentials: true,
-}));
+app.use(cors());
 
 const mongoOptions = {
     useNewUrlParser: true,
