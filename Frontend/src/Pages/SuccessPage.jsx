0e1a7fe76499bc9c87a883dff655a4e377abd35d
junaid-arif-24 <mohdjunaid7078@gmail.com>
2023-09-23 03:32:51 +0530

    added axios

--- Frontend/src/Pages/SuccessPage.jsx
+++ Frontend/src/Pages/SuccessPage.jsx
@@ -1,27 +1,28 @@
-import {useEffect} from 'react'
-import { useNavigate } from 'react-router-dom';
+import {useEffect} from 'react';
+import {useNavigate} from 'react-router-dom';
 import {useRecoilState} from 'recoil';
+import axios from 'axios';
 import {userAtom} from '../store/userAtom';
 const SuccessPage = () => {
   const [, setUsername] = useRecoilState(userAtom);
   const navigate = useNavigate();
+
   const fetchAuthUser = async () => {
     try {
-      const response = await fetch(
+      const response = await axios.get(
         `${import.meta.env.VITE_BACKEND_URL}/auth/user`,
         {
-          method: 'GET',
-          credentials: 'include',
+          withCredentials: true,
         },
       );
 
-      if (response.ok) {
-        const data = await response.json();
+      if (response.status === 200) {
+        const data = response.data;
         console.log('User: ', data);
         const newUsername = data.fullname;
         localStorage.setItem('username', newUsername);
-        setUsername(newUsername);
-        navigate('/concern');
+        setUsername(newUsername); 
+        navigate('/concern'); 
       } else {
         console.log('Not properly authenticated');
       }
@@ -29,15 +30,14 @@ const SuccessPage = () => {
       console.error('Error:', error);
     }
   };
-    useEffect(() => {
-        setTimeout(() => {
-          fetchAuthUser();
-        }, 1000);
-      }, []);
-    
-  return (
-    <h2>You are successfully logged in</h2>
-  )
-}
 
-export default SuccessPage
+  useEffect(() => {
+    setTimeout(() => {
+      fetchAuthUser();
+    }, 1000);
+  }, []);
+
+  return <h2>You are successfully logged in</h2>;
+};
+
+export default SuccessPage;
